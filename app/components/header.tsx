ac3d32ec689d5e0e66a32e0f497c5670c83657ef
Yama-App <chameleonjapanapp@gmail.com>
2026-06-27 17:37:47 +0900

    change favicon

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,11 +6,11 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <header className="relative z-50 w-full border-b border-zinc-200 bg-white backdrop-blur-sm dark:border-zinc-800 dark:bg-zinc-900 dark:backdrop-blur-none">
-      <div className="mx-auto flex w-full items-center justify-start px-4 py-2">
+      <div className="mx-auto flex w-full items-center justify-start px-4 py-1">
         {/* Left: Logo + Title */}
         <Link href="/" className="flex items-center gap-2">
-          <Image src="/yama-app.svg" alt="yamaapp logo" width={48} height={48} />
-          <span className="text-xl font-black text-zinc-800 dark:text-zinc-100">Yama-App</span>
+          <Image src="/yama-app.svg" alt="yamaapp logo" width={32} height={32} />
+          <span className="text-base font-black text-zinc-800 dark:text-zinc-100">Yama-App</span>
         </Link>
       </div>
     </header>
